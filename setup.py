c08275e6b1fdcf5e4ac7e0ad774b93630943b559
qdewaghe <q.dewaghe@gmail.com>
2020-10-30 15:45:00 +0100

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='compile-commands',
-    version='1.0.2',
+    version='1.0.3',
     url="https://github.com/qdewaghe/compile-commands",
     author="Quentin Dewaghe",
     author_email="q.dewaghe@gmail.com",
